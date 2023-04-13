7ea800ca505aeaf2fae8d143a20a0c6204a9908b
PascalCorpsman <corpsman@corpsman.de>
2023-04-13 05:31:23 +0200

    ADD: Start with scheme converting

--- cd_data_extractor/ucdextractor.pas
+++ cd_data_extractor/ucdextractor.pas
@@ -8,14 +8,91 @@ Uses
   Classes, SysUtils;
 
 Function CheckCDRootFolder(aFolder: String): boolean;
+Function CheckFPCAtomicFolder(aFolder: String): boolean;
 
+Procedure ExtractAtomicImages(CDFolder, AtomicFolder: String);
+Procedure ExtractAtomicSounds(CDFolder, AtomicFolder: String);
+Procedure ExtractAtomicShemes(CDFolder, AtomicFolder: String);
 
 Implementation
 
+Uses
+  FileUtil, Unit1;
+
 Function CheckCDRootFolder(aFolder: String): boolean;
+Var
+  sl: TStringList;
+  checks: Array[0..3] Of Boolean;
+  i: integer;
 Begin
+  result := true;
+  sl := FindAllDirectories(aFolder, True);
+  For i := 0 To high(checks) Do Begin
+    checks[i] := false;
+  End;
+  For i := 0 To sl.Count - 1 Do Begin
+    If pos('data' + PathDelim + 'ani', lowercase(sl[i])) <> 0 Then checks[0] := true;
+    If pos('data' + PathDelim + 'res', lowercase(sl[i])) <> 0 Then checks[1] := true;
+    If pos('data' + PathDelim + 'schemes', lowercase(sl[i])) <> 0 Then checks[2] := true;
+    If pos('data' + PathDelim + 'sound', lowercase(sl[i])) <> 0 Then checks[3] := true;
+  End;
+  For i := 0 To high(checks) Do Begin
+    result := result And checks[i];
+  End;
+  sl.free;
+End;
+
+Function CheckFPCAtomicFolder(aFolder: String): boolean;
+Var
+  s: String;
+Begin
+  s := 'fpc_atomic';
+{$IFDEF Windows}
+  s := s + '.exe';
+{$ENDIF}
+  result := FileExists(IncludeTrailingPathDelimiter(aFolder) + s);
+End;
+
+Function GetFolderByMatch(RootFolder, Match: String): String;
+Var
+  sl: TStringList;
+  i: Integer;
+Begin
+  result := '';
+  sl := FindAllDirectories(RootFolder, True);
+  For i := 0 To sl.Count - 1 Do Begin
+    If pos(match, lowercase(sl[i])) <> 0 Then Begin
+      result := sl[i];
+      break;
+    End;
+  End;
+  sl.free;
+End;
+
+Procedure ExtractAtomicImages(CDFolder, AtomicFolder: String);
+Begin
+  AddLog('  not implemented yet, sry.');
+End;
+
+Procedure ExtractAtomicSounds(CDFolder, AtomicFolder: String);
+Begin
+  AddLog('  not implemented yet, sry.');
+End;
+
+Procedure ExtractAtomicShemes(CDFolder, AtomicFolder: String);
+Var
+  SchemeFolder: String;
+Begin
+  SchemeFolder := GetFolderByMatch(CDFolder, 'data' + PathDelim + 'schemes');
+  If SchemeFolder = '' Then Begin
+    addlog('  Error, unable to find schemes folder.');
+  End;
+
+  hier weiter
 
 End;
 
 End.
+
+
 
--- cd_data_extractor/unit1.pas
+++ cd_data_extractor/unit1.pas
@@ -15,11 +15,20 @@ Type
   TForm1 = Class(TForm)
     Button1: TButton;
     Button2: TButton;
+    Button3: TButton;
+    Button4: TButton;
+    Button5: TButton;
     IniPropStorage1: TIniPropStorage;
     Label1: TLabel;
+    Label2: TLabel;
+    Label3: TLabel;
+    Memo1: TMemo;
     SelectDirectoryDialog1: TSelectDirectoryDialog;
     Procedure Button1Click(Sender: TObject);
     Procedure Button2Click(Sender: TObject);
+    Procedure Button3Click(Sender: TObject);
+    Procedure Button4Click(Sender: TObject);
+    Procedure Button5Click(Sender: TObject);
     Procedure FormCloseQuery(Sender: TObject; Var CanClose: Boolean);
     Procedure FormCreate(Sender: TObject);
   private
@@ -31,11 +40,20 @@ Type
 Var
   Form1: TForm1;
 
+Procedure AddLog(aLog: String);
+
 Implementation
 
 {$R *.lfm}
 
 Uses ucdextractor;
+
+Procedure AddLog(aLog: String);
+Begin
+  form1.Memo1.Lines.Add(aLog);
+  form1.Memo1.SelStart := length(Form1.Memo1.Text); // Scroll down to see newest entry
+  Application.ProcessMessages;
+End;
 
 { TForm1 }
 
@@ -43,11 +61,11 @@ Procedure TForm1.FormCreate(Sender: TObject);
 Begin
   IniPropStorage1.IniFileName := 'settings.ini';
   caption := 'FPC Atomic data extractor ver. 0.01';
-  Constraints.MinHeight := Height;
-  Constraints.MaxHeight := Height;
-  Constraints.MinWidth := Width;
-  Constraints.MaxWidth := Width;
   label1.caption := IniPropStorage1.ReadString('CD-Root', '');
+  label2.caption := IniPropStorage1.ReadString('FPC-Atomic', '');
+  memo1.clear;
+  Constraints.MinWidth := Width;
+  Constraints.MinHeight := Height;
 End;
 
 Procedure TForm1.Button2Click(Sender: TObject);
@@ -55,9 +73,49 @@ Begin
   close;
 End;
 
+Procedure TForm1.Button3Click(Sender: TObject);
+Begin
+  showmessage('Click the buttons from top to button, carefully read error messages and solve them.' + LineEnding + LineEnding +
+    'If you master to come to the last button without errors you can enjoy the game *g*.');
+End;
+
+Procedure TForm1.Button4Click(Sender: TObject);
+Begin
+  If SelectDirectoryDialog1.Execute Then Begin
+    If CheckFPCAtomicFolder(SelectDirectoryDialog1.FileName) Then Begin
+      label2.Caption := SelectDirectoryDialog1.FileName;
+    End
+    Else Begin
+      showmessage('Error, the folder should at least contain the executable for fpc_atomic');
+    End;
+  End;
+End;
+
+Procedure TForm1.Button5Click(Sender: TObject);
+Begin
+  AddLog('Start');
+  // Start extraction
+  If Not CheckCDRootFolder(label1.Caption) Then Begin
+    AddLog('Error: invalid atomic bomberman CD-Image folder');
+    exit;
+  End;
+  If Not CheckFPCAtomicFolder(label2.Caption) Then Begin
+    AddLog('Error: invalid fpc-atomic folder');
+    exit;
+  End;
+  AddLog('Images');
+  ExtractAtomicImages(Label1.caption, label2.caption);
+  AddLog('Sounds');
+  ExtractAtomicSounds(Label1.caption, label2.caption);
+  AddLog('Shemes');
+  ExtractAtomicShemes(Label1.caption, label2.caption);
+  AddLog('Done, please check results.');
+End;
+
 Procedure TForm1.FormCloseQuery(Sender: TObject; Var CanClose: Boolean);
 Begin
   IniPropStorage1.WriteString('CD-Root', label1.caption);
+  IniPropStorage1.WriteString('FPC-Atomic', label2.caption);
 End;
 
 Procedure TForm1.Button1Click(Sender: TObject);
